6c8d4dbe3e5b7f7a734edc1f8e9234ea32662370
aa3447 <jacksonjam1@yahoo.com>
2025-02-21 20:19:13 -0500

    Start of text processing with textnode

---
+++ src/textnode.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self , text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def get_text(self):
+        return self.text
+    
+    def get_text_type(self):
+        return self.text_type
+    
+    def get_url(self):
+        return self.url
+
+    def __eq__(self, text_node):
+        return self.text == text_node.get_text() and self.text_type == text_node.get_text_type() and self.url == text_node.get_url()
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
